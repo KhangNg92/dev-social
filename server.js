34aab2a7e8433fe7f2a6c94ea9a29668de02e5df
Khang Nguyen <mkavo92@gmail.com>
2018-09-28 13:14:44 -0500

    fixing hrk

--- server.js
+++ server.js
@@ -1,49 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const path = require('path');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
-const passport = require('passport');
-const path = require('path')
+
 const app = express();
 
-//Body parser middleware
-
-app.use(bodyParser.urlencoded({ extended: false}));
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-//DB Config
 
+// DB Config
 const db = require('./config/keys').mongoURI;
 
-//Connect to MongoDB
+// Connect to MongoDB
 mongoose
-.connect(db)
-.then(() => console.log('MongoDB Connected'))
-.catch(err => console.log(err));
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-
-//passport middleware
+// Passport middleware
 app.use(passport.initialize());
 
-//passport config
+// Passport Config
 require('./config/passport')(passport);
 
-//Use routes
+// Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
 app.use('/api/posts', posts);
 
 // Server static assets if in production
-if(process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static('client/build'));
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
 
-    app.get('*', (req, res) => {
-        res.sendfile(path.resolve(__dirname), 'client', 'build', 'index.html')
-    })
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
 }
+
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Sever running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
